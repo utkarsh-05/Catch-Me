77beea545865a1056d5b5501561e870f52acf321
Utkarsh Seksaria <utkarshseksaria@gmail.comexitexit()exitgit config --global user.email utkarshseksaria@gmail.com>
2021-04-15 23:02:56 +0530

    Add frontend and js

---
+++ logic.js
@@ -0,0 +1,8 @@
+var box = document.getElementById('box');
+box.addEventListener('mouseover', function(){
+    var top = Math.floor((Math.random()*100)+1);
+    console.log('mouse hover ');
+    box.style.marginTop= top + "vh";
+    var left =  Math.floor((Math.random()*90)+1);
+    box.style.marginLeft = left + "vw";
+})
