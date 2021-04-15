2db399aa0e2a6adef2a4d57cc0cda219e6ca3b7c
Utkarsh Seksaria <utkarshseksaria@gmail.comexitexit()exitgit config --global user.email utkarshseksaria@gmail.com>
2021-04-15 23:18:50 +0530

    Fix scrolling

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 var box = document.getElementById('box');
 box.addEventListener('mouseover', function(){
-    var top = Math.floor((Math.random()*100)+1);
+    var top = Math.floor((Math.random()*90)+1);
     console.log('mouse hover ');
     box.style.marginTop= top + "vh";
     var left =  Math.floor((Math.random()*90)+1);
